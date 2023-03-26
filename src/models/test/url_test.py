3ea953b46e704e6b25313f056050b75d8b12f720
Ihab Abdelkareem <ihababdelkareem2017@gmail.com>
2023-03-26 16:08:16 +0000

    additions

--- src/models/test/url_test.py
+++ src/models/test/url_test.py
@@ -40,3 +40,26 @@ def test_url_address(test_address):
         test_address (str): test address for URL
     """
     assert URL(test_address).address == test_address
+
+
+@pytest.mark.parametrize(
+    "first_url, second_url, expected_equality",
+    [
+        (URL("https://www.google.com/a/b"), URL("https://www.google.com/a/b"), True),
+        (URL("https://www.google.com/a/b"), URL("https://www.google.com/b/"), False),
+        (URL("https://www.google.com/a/b"), "https://www.google.com/a/b", False),
+    ],
+)
+def test_url_hash(first_url, second_url, expected_equality):
+    """
+    Verify that two URLs are considered duplicate when hashed into a set
+    in case they are both URL objects and have the same address hash.
+
+    Args:
+        test_address (str): test address for URL
+    """
+    set_ = set()
+    set_.add(first_url)
+    set_.add(second_url)
+
+    assert (len(set_) == 1) == expected_equality
